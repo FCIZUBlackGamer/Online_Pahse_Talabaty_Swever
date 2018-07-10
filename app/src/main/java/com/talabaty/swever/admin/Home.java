293fb0886598705c5c6db37466d5d70a0194abbb
fci <momen.shahen2020@gmail.com>
2018-07-10 17:27:23 +0200

    Fill Some Data in Tables

--- app/src/main/java/com/talabaty/swever/admin/Home.java
+++ app/src/main/java/com/talabaty/swever/admin/Home.java
@@ -16,6 +16,35 @@ import com.talabaty.swever.admin.Mabi3at.MainHome;
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+/*
+.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        getContext(),
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                        , DatePicker1
+                        , year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+            }
+
+        });
+        DatePicker1 = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                dateFrom = month + "/" + dayOfMonth + "/" + year;
+                txtFrom.setText(dateFrom);
+            }
+        };
+* */
+
 
     NavigationView navigationView;
 //    Fragment fragment;
